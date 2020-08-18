211b3c344ddebacbe083cf2e5b4c45394de3969b
Max Schmitt <max@schmitt.mx>
2020-08-18 23:08:11 +0200

    fix: cleanup

--- run.go
+++ run.go
@@ -33,40 +33,51 @@ func getDriverURL() (string, string) {
 	return fmt.Sprintf("%s%s/%s", baseURL, version, driverName), fmt.Sprintf("%s-%s", driverName, hex.EncodeToString(hash.Sum(nil))[:5])
 }
 
-func installDriver() (string, error) {
+func installPlaywright() (string, error) {
 	driverURL, driverName := getDriverURL()
 	driverPath := filepath.Join(os.TempDir(), driverName)
-	if _, err := os.Stat(driverPath); os.IsNotExist(err) {
-		log.Println("Downloading driver...")
-		resp, err := http.Get(driverURL)
-		if err != nil {
-			return "", fmt.Errorf("could not download driver: %v", err)
-		}
-		if resp.StatusCode != http.StatusOK {
-			return "", fmt.Errorf("error: got non 2xx status code: %d (%s)", resp.StatusCode, resp.Status)
-		}
-		outFile, err := os.Create(driverPath)
-		if err != nil {
-			return "", fmt.Errorf("could not create driver: %v", err)
-		}
-		if _, err = io.Copy(outFile, resp.Body); err != nil {
-			return "", fmt.Errorf("could not copy response body to file: %v", err)
-		}
-		if err := outFile.Close(); err != nil {
-			return "", fmt.Errorf("could not close file (driver): %v", err)
-		}
-
-		if runtime.GOOS != "windows" {
-			stats, err := os.Stat(driverPath)
-			if err != nil {
-				return "", fmt.Errorf("could not stat driver: %v", err)
-			}
-			if err := os.Chmod(driverPath, stats.Mode()|0x40); err != nil {
-				return "", fmt.Errorf("could not set permissions: %v", err)
-			}
-		}
-		log.Println("Downloaded driver successfully")
+	_, err := os.Stat(driverPath)
+	if err == nil {
+		return driverPath, nil
 	}
+	if !os.IsNotExist(err) {
+		return driverName, err
+	}
+	log.Println("Downloading driver...")
+	resp, err := http.Get(driverURL)
+	if err != nil {
+		return "", fmt.Errorf("could not download driver: %v", err)
+	}
+	if resp.StatusCode != http.StatusOK {
+		return "", fmt.Errorf("error: got non 2xx status code: %d (%s)", resp.StatusCode, resp.Status)
+	}
+	outFile, err := os.Create(driverPath)
+	if err != nil {
+		return "", fmt.Errorf("could not create driver: %v", err)
+	}
+	if _, err = io.Copy(outFile, resp.Body); err != nil {
+		return "", fmt.Errorf("could not copy response body to file: %v", err)
+	}
+	if err := outFile.Close(); err != nil {
+		return "", fmt.Errorf("could not close file (driver): %v", err)
+	}
+
+	if runtime.GOOS != "windows" {
+		stats, err := os.Stat(driverPath)
+		if err != nil {
+			return "", fmt.Errorf("could not stat driver: %v", err)
+		}
+		if err := os.Chmod(driverPath, stats.Mode()|0x40); err != nil {
+			return "", fmt.Errorf("could not set permissions: %v", err)
+		}
+	}
+	log.Println("Downloaded driver successfully")
+
+	log.Println("Downloading browsers...")
+	if err := installBrowsers(driverPath); err != nil {
+		return "", fmt.Errorf("could not install browsers: %v", err)
+	}
+	log.Println("Downloaded browsers successfully")
 	return driverPath, nil
 }
 
@@ -77,30 +88,27 @@ func installBrowsers(driverPath string) error {
 	if err := cmd.Start(); err != nil {
 		return fmt.Errorf("could not start driver: %v", err)
 	}
-	return cmd.Wait()
+	if err := cmd.Wait(); err != nil {
+		return err
+	}
+	return nil
 }
 
 // Install does download the driver and the browsers. If not called manually
 // before playwright.Run() it will get executed there and might take a few seconds
 // to download the Playwright suite.
 func Install() error {
-	driverPath, err := installDriver()
+	_, err := installPlaywright()
 	if err != nil {
 		return fmt.Errorf("could not install driver: %v", err)
-	}
-	if err := installBrowsers(driverPath); err != nil {
-		return fmt.Errorf("could not install browsers: %v", err)
 	}
 	return nil
 }
 
 func Run() (*Playwright, error) {
-	driverPath, err := installDriver()
+	driverPath, err := installPlaywright()
 	if err != nil {
 		return nil, fmt.Errorf("could not install driver: %v", err)
-	}
-	if err := installBrowsers(driverPath); err != nil {
-		return nil, fmt.Errorf("could not install browsers: %v", err)
 	}
 
 	cmd := exec.Command(driverPath, "--run")
