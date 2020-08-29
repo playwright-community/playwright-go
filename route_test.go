2ac6d902bc20acae9ffa22b45ac9eb3cb3310db8
Max Schmitt <max@schmitt.mx>
2020-08-29 17:34:38 +0200

    fix: it

--- route_test.go
+++ route_test.go
@@ -10,7 +10,7 @@ func TestRouteContinue(t *testing.T) {
 	helper := NewTestHelper(t)
 	defer helper.AfterEach()
 	intercepted := make(chan bool, 1)
-	helper.Page.Route("**/empty.html", func(route *Route, request *Request) {
+	err := helper.Page.Route("**/empty.html", func(route *Route, request *Request) {
 		require.Equal(t, route.Request(), request)
 		require.Contains(t, request.URL(), "empty.html")
 		require.Greater(t, 5, len(request.Headers()["User-Agent"]))
@@ -23,9 +23,10 @@ func TestRouteContinue(t *testing.T) {
 		require.Equal(t, "document", request.ResourceType())
 		require.Equal(t, request.Frame(), helper.Page.mainFrame)
 		require.Equal(t, "about:blank", request.Frame().URL())
-		route.Continue()
+		require.NoError(t, route.Continue())
 		intercepted <- true
 	})
+	require.NoError(t, err)
 	response, err := helper.Page.Goto(helper.server.EMPTY_PAGE)
 	require.NoError(t, err)
 	require.True(t, response.Ok())
