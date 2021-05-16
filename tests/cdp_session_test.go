6a51d5d3edacd8bfd0ac762bf9e95d6c763b6f59
Jiong <jiong.han@zoho.com>
2021-05-15 22:18:35 -0700

    make cdpsession test only available on chromium

--- tests/cdp_session_test.go
+++ tests/cdp_session_test.go
@@ -10,32 +10,44 @@ func TestCDPSessionSend(t *testing.T) {
 	BeforeEach(t)
 	defer AfterEach(t)
 	cdpSession, err := browser.NewBrowserCDPSession()
-	require.NoError(t, err)
-	result, err := cdpSession.Send("Target.getTargets", nil)
-	require.NoError(t, err)
-	targetInfos := result.(map[string]interface{})["targetInfos"].([]interface{})
-	require.Equal(t, 1, len(targetInfos))
+	if isChromium {
+		require.NoError(t, err)
+		result, err := cdpSession.Send("Target.getTargets", nil)
+		require.NoError(t, err)
+		targetInfos := result.(map[string]interface{})["targetInfos"].([]interface{})
+		require.Equal(t, 1, len(targetInfos))
+	} else {
+		require.Error(t, err)
+	}
 }
 
 func TestCDPSessionOn(t *testing.T) {
 	BeforeEach(t)
 	defer AfterEach(t)
 	cdpSession, err := page.Context().NewCDPSession(page)
-	require.NoError(t, err)
-	_, err = cdpSession.Send("Console.enable", nil)
-	require.NoError(t, err)
-	cdpSession.On("Console.messageAdded", func(params map[string]interface{}) {
-		require.NotNil(t, params)
-	})
-	_, err = page.Evaluate(`console.log("hello")`)
-	require.NoError(t, err)
-	require.NoError(t, cdpSession.Detach())
+	if isChromium {
+		require.NoError(t, err)
+		_, err = cdpSession.Send("Console.enable", nil)
+		require.NoError(t, err)
+		cdpSession.On("Console.messageAdded", func(params map[string]interface{}) {
+			require.NotNil(t, params)
+		})
+		_, err = page.Evaluate(`console.log("hello")`)
+		require.NoError(t, err)
+		require.NoError(t, cdpSession.Detach())
+	} else {
+		require.Error(t, err)
+	}
 }
 
 func TestCDPSessionDetach(t *testing.T) {
 	BeforeEach(t)
 	defer AfterEach(t)
 	cdpSession, err := browser.NewBrowserCDPSession()
-	require.NoError(t, err)
-	require.NoError(t, cdpSession.Detach())
+	if isChromium {
+		require.NoError(t, err)
+		require.NoError(t, cdpSession.Detach())
+	} else {
+		require.Error(t, err)
+	}
 }
