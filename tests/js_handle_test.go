41cae5ded8fae76c62460a6fead885839df91b0c
Max Schmitt <max@schmitt.mx>
2021-07-29 13:24:11 +0200

    chore: fix linting warnings (#148)

--- tests/js_handle_test.go
+++ tests/js_handle_test.go
@@ -3,7 +3,6 @@ package playwright_test
 import (
 	"testing"
 
-	"github.com/mxschmitt/playwright-go"
 	"github.com/stretchr/testify/require"
 )
 
@@ -16,7 +15,7 @@ func TestJSHandleGetProperty(t *testing.T) {
 		three: 3
 	})`)
 	require.NoError(t, err)
-	twoHandle, err := aHandle.(playwright.JSHandle).GetProperty("two")
+	twoHandle, err := aHandle.GetProperty("two")
 	require.NoError(t, err)
 	value, err := twoHandle.JSONValue()
 	require.NoError(t, err)
@@ -32,7 +31,7 @@ func TestJSHandleGetProperties(t *testing.T) {
 		three: 3
 	})`)
 	require.NoError(t, err)
-	twoHandle, err := aHandle.(playwright.JSHandle).GetProperties()
+	twoHandle, err := aHandle.GetProperties()
 	require.NoError(t, err)
 	v1, err := twoHandle["one"].JSONValue()
 	require.NoError(t, err)
@@ -56,7 +55,7 @@ func TestJSHandleEvaluate(t *testing.T) {
 		three: 3
 	})`)
 	require.NoError(t, err)
-	twoHandle, err := aHandle.(playwright.JSHandle).Evaluate("x => x")
+	twoHandle, err := aHandle.Evaluate("x => x")
 	require.NoError(t, err)
 	values := twoHandle.(map[string]interface{})
 	require.Equal(t, 1, values["one"])
@@ -73,11 +72,11 @@ func TestJSHandleEvaluateHandle(t *testing.T) {
 		three: 3
 	})`)
 	require.NoError(t, err)
-	twoHandle, err := aHandle.(playwright.JSHandle).EvaluateHandle("x => x")
+	twoHandle, err := aHandle.EvaluateHandle("x => x")
 	require.NoError(t, err)
-	twoHandle, err = twoHandle.(playwright.JSHandle).GetProperty("two")
+	twoHandle, err = twoHandle.GetProperty("two")
 	require.NoError(t, err)
-	value, err := twoHandle.(playwright.JSHandle).JSONValue()
+	value, err := twoHandle.JSONValue()
 	require.NoError(t, err)
 	require.Equal(t, value, 2)
 }
@@ -91,10 +90,10 @@ func TestJSHandleTypeParsing(t *testing.T) {
 		a_string_of_an_integer: "3",
 	})`)
 	require.NoError(t, err)
-	twoHandle, err := aHandle.(playwright.JSHandle).EvaluateHandle("x => x")
+	twoHandle, err := aHandle.EvaluateHandle("x => x")
 	require.NoError(t, err)
 
-	integerHandle, err := twoHandle.(playwright.JSHandle).GetProperty("an_integer")
+	integerHandle, err := twoHandle.GetProperty("an_integer")
 	require.NoError(t, err)
 	intV, err := integerHandle.JSONValue()
 	require.NoError(t, err)
@@ -103,7 +102,7 @@ func TestJSHandleTypeParsing(t *testing.T) {
 	_, ok = intV.(float64)
 	require.False(t, ok)
 
-	floatHandle, err := twoHandle.(playwright.JSHandle).GetProperty("a_float")
+	floatHandle, err := twoHandle.GetProperty("a_float")
 	require.NoError(t, err)
 	floatV, err := floatHandle.JSONValue()
 	require.NoError(t, err)
@@ -112,7 +111,7 @@ func TestJSHandleTypeParsing(t *testing.T) {
 	_, ok = floatV.(int)
 	require.False(t, ok)
 
-	stringHandle, err := twoHandle.(playwright.JSHandle).GetProperty("a_string_of_an_integer")
+	stringHandle, err := twoHandle.GetProperty("a_string_of_an_integer")
 	require.NoError(t, err)
 	stringV, err := stringHandle.JSONValue()
 	require.NoError(t, err)
