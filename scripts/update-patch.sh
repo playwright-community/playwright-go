298e69d7ce22ebadda11d2dbd26fefe309f9c518
Kumar Aditya <59607654+kumaraditya303@users.noreply.github.com>
2021-07-30 09:37:49 +0530

    chore: roll to 1.11

--- scripts/update-patch.sh
+++ scripts/update-patch.sh
@@ -6,8 +6,8 @@ cd "$(dirname "$0")"
 
 cd ../playwright 
 
-git diff --diff-algorithm=myers --full-index --staged > ../patches/main.patch
+git diff --full-index --src-prefix="a/playwright/" --dst-prefix="b/playwright/" > ../patches/main.patch $(git rev-parse HEAD^1)..$(git rev-parse HEAD)
 
-git reset --hard
+git reset --hard $(git rev-parse HEAD^1)
 
-cd - 
+cd -
