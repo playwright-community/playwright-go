791647425dd0bc89a5099a6841fc9585d645ef82
Max Schmitt <max@schmitt.mx>
2021-08-21 14:15:53 +0200

    devops: rework patch infra scripts (#164)

--- scripts/apply-patch.sh
+++ scripts/apply-patch.sh
@@ -1,19 +1,30 @@
 #!/bin/bash
 
+set -e
+
+BRANCH_NAME_HEAD="playwright-head"
+BRANCH_NAME_BUILD="playwright-build"
+SCRIPTS_DIR="$(dirname "$0")"
+
 echo "Applying patches..."
 
-cd "$(dirname "$0")"
-
-cd ..
-
-git apply --whitespace=nowarn patches/*
-
+pushd "$SCRIPTS_DIR/.."
+git submodule update --init
 pushd playwright
 
-git add -A
+git reset --hard HEAD
 
+if git show-ref -q --heads "$BRANCH_NAME_HEAD"; then
+  git branch -D "$BRANCH_NAME_HEAD"
+fi
+if git show-ref -q --heads "$BRANCH_NAME_BUILD"; then
+  git branch -D "$BRANCH_NAME_BUILD"
+fi
+
+git checkout -b "$BRANCH_NAME_HEAD"
+git checkout -b "$BRANCH_NAME_BUILD"
+git apply --whitespace=nowarn ../patches/*
+git add -A
 git commit -m "Applied patches"
 
 popd
-
-cd -
