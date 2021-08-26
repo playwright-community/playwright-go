4d4d66041c299f6c921227d7008e350622e3a5c0
Kumar Aditya <59607654+kumaraditya303@users.noreply.github.com>
2021-08-26 15:03:14 +0530

    chore: add backgroundpage (#199)
    * chore: add backgroundpage
    
    * test: add tests
    
    * chore: fix it

--- browser_context.go
+++ browser_context.go
@@ -19,6 +19,7 @@ type browserContextImpl struct {
 	ownedPage         Page
 	browser           *browserImpl
 	serviceWorkers    []*workerImpl
+	backgroundPages   []Page
 	bindings          map[string]BindingCallFunction
 	tracing           *tracingImpl
 }
@@ -364,6 +365,21 @@ func (p *browserContextImpl) Pause() error {
 	return err
 }
 
+func (b *browserContextImpl) OnBackgroundPage(ev map[string]interface{}) {
+	b.Lock()
+	p := fromChannel(ev["page"]).(*pageImpl)
+	p.browserContext = b
+	b.backgroundPages = append(b.backgroundPages, p)
+	b.Unlock()
+	b.Emit("backgroundpage", p)
+}
+
+func (b *browserContextImpl) BackgroundPages() []Page {
+	b.Lock()
+	defer b.Unlock()
+	return b.backgroundPages
+}
+
 func newBrowserContext(parent *channelOwner, objectType string, guid string, initializer map[string]interface{}) *browserContextImpl {
 	bt := &browserContextImpl{
 		timeoutSettings: newTimeoutSettings(nil),
@@ -423,5 +439,6 @@ func newBrowserContext(parent *channelOwner, objectType string, guid string, ini
 	bt.channel.On("route", func(params map[string]interface{}) {
 		bt.onRoute(fromChannel(params["route"]).(*routeImpl), fromChannel(params["request"]).(*requestImpl))
 	})
+	bt.channel.On("backgroundPage", bt.OnBackgroundPage)
 	return bt
 }
