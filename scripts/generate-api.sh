1134dbc23f90b74fcff9470b807c7c54f8b0f93f
Kumar Aditya <59607654+kumaraditya303@users.noreply.github.com>
2021-09-23 12:48:10 +0530

    chore: use go generate

--- scripts/generate-api.sh
+++ scripts/generate-api.sh
@@ -7,26 +7,16 @@ PWD=$(pwd -P)
 $PWD/scripts/apply-patch.sh
 
 echo "Generating Interfaces"
-node scripts/generate-interfaces.js > generated_interfaces.go
-go fmt generated_interfaces.go > /dev/null
+node scripts/generate-interfaces.js > generated-interfaces.go
+go fmt generated-interfaces.go > /dev/null
 echo "Generated Interfaces"
 
 PLAYWRIGHT_DIR="playwright"
 
 node $PLAYWRIGHT_DIR/utils/doclint/generateGoApi.js
 mv $PLAYWRIGHT_DIR/utils/doclint/generate_types/go/generated-{enums,structs}.go .
-rm $PLAYWRIGHT_DIR/utils/doclint/generate_types/go/generated-interfaces.go
 go fmt generated-{enums,structs}.go > /dev/null
 
-# echo "Validating API"
-# node scripts/validate-interfaces.js
-# echo "Validated API"
-
-# echo "Updating README"
+echo "Updating README"
 go run scripts/update-readme-versions/main.go
-
-pushd $PLAYWRIGHT_DIR
-
-git reset --hard $(git rev-parse playwright-build^1)
-
-popd
+git submodule update
