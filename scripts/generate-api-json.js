f315239c0ef4179ee803758759cbd04fe0864c21
Kumar Aditya <59607654+kumaraditya303@users.noreply.github.com>
2021-10-04 15:39:03 +0530

    chore: use go generate (#208)

--- scripts/generate-api-json.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-const fs = require("fs")
-const path = require("path")
-const { getAPIDocs } = require("./helpers")
-
-const api = getAPIDocs()
-
-fs.writeFileSync(path.join(__dirname, "..", "api.json"), JSON.stringify(api, null, 2))
