7027e1387f374e4d491867138bbf129da31f080d
canstand <canstand@users.noreply.github.com>
2023-10-25 07:08:25 +0000

    deploy: 781b80d3e4fa09e1e75539831149070c7ed9ecc0

--- element_handle.go
+++ element_handle.go
@@ -226,10 +226,29 @@ func (e *elementHandleImpl) SelectText(options ...ElementHandleSelectTextOptions
 
 func (e *elementHandleImpl) Screenshot(options ...ElementHandleScreenshotOptions) ([]byte, error) {
 	var path *string
-	if len(options) > 0 {
+	overrides := map[string]interface{}{}
+	if len(options) == 1 {
 		path = options[0].Path
+		options[0].Path = nil
+		if options[0].Mask != nil {
+			masks := make([]map[string]interface{}, 0)
+			for _, m := range options[0].Mask {
+				if m.Err() != nil { // ErrLocatorNotSameFrame
+					return nil, m.Err()
+				}
+				l, ok := m.(*locatorImpl)
+				if ok {
+					masks = append(masks, map[string]interface{}{
+						"selector": l.selector,
+						"frame":    l.frame.channel,
+					})
+				}
+			}
+			overrides["mask"] = masks
+			options[0].Mask = nil
+		}
 	}
-	data, err := e.channel.Send("screenshot", options)
+	data, err := e.channel.Send("screenshot", options, overrides)
 	if err != nil {
 		return nil, fmt.Errorf("could not send message :%w", err)
 	}
