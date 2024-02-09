3806b87dec58b27f695e306dc2633c9bf8da2071
canstand <canstand@users.noreply.github.com>
2024-02-09 14:56:53 +0000

    deploy: a6f1d2148476e1c7584f421aca307dc0f122f8f9

--- tests/browser_context_storage_state_test.go
+++ tests/browser_context_storage_state_test.go
@@ -144,3 +144,52 @@ func TestBrowserContextStorageStateRoundTripThroughTheFile(t *testing.T) {
 	require.NoError(t, err)
 	require.Equal(t, map[string]interface{}{"name1": "value1"}, localStorage)
 }
+
+func TestBrowserContextStorageStateRoundTripThroughConvert(t *testing.T) {
+	BeforeEach(t)
+	defer AfterEach(t)
+	page1, err := context.NewPage()
+	require.NoError(t, err)
+	defer page1.Close()
+	require.NoError(t, page1.Route("**/*", func(route playwright.Route) {
+		require.NoError(t, route.Fulfill(playwright.RouteFulfillOptions{
+			Body: "<html></html>",
+		}))
+	}))
+	_, err = page1.Goto("https://www.example.com")
+	require.NoError(t, err)
+	_, err = page1.Evaluate(`
+	() => {
+		localStorage["name1"] = "value1"
+		document.cookie = "username=John Doe"
+		return document.cookie
+	}
+	`)
+	require.NoError(t, err)
+
+	storageState, err := context.StorageState()
+	require.NoError(t, err)
+
+	context2, err := browser.NewContext(
+		playwright.BrowserNewContextOptions{
+			StorageState: storageState.ToOptionalStorageState(),
+		})
+	require.NoError(t, err)
+	defer context2.Close()
+	page2, err := context2.NewPage()
+	require.NoError(t, err)
+	defer page2.Close()
+	require.NoError(t, page2.Route("**/*", func(route playwright.Route) {
+		require.NoError(t, route.Fulfill(playwright.RouteFulfillOptions{
+			Body: "<html></html>",
+		}))
+	}))
+	_, err = page2.Goto("https://www.example.com")
+	require.NoError(t, err)
+	cookie, err := page2.Evaluate("document.cookie")
+	require.NoError(t, err)
+	require.Equal(t, "username=John Doe", cookie)
+	localStorage, err := page2.Evaluate("window.localStorage")
+	require.NoError(t, err)
+	require.Equal(t, map[string]interface{}{"name1": "value1"}, localStorage)
+}
